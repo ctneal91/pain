076413fc950f0542d4138f4b9cdc44ad8836c25f
Christian Neal <c.neal91@gmail.com>
2015-11-11 12:08:59 -0600

    Drop remove drug id from migrations

--- db/migrate/20151104212152_drop_drug_id_and_add_drug_names_to_prescriptions.rb
+++ db/migrate/20151104212152_drop_drug_id_and_add_drug_names_to_prescriptions.rb
@@ -1,6 +1,5 @@
 class DropDrugIdAndAddDrugNamesToPrescriptions < ActiveRecord::Migration
   def change
-    remove_column :prescriptions, :drug_id, :integer
     add_column :prescriptions, :drug_name, :integer
     add_column :prescriptions, :purpose, :string
     add_column :prescriptions, :instructions, :string
