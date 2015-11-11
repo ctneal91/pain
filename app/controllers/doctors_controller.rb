45870e5ba7446d75ca7d6a8854152de8999ceff6
Christian Neal <c.neal91@gmail.com>
2015-11-11 15:57:13 -0600

    Change all header and titlebars to partials

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -29,7 +29,7 @@ class DoctorsController < ApplicationController
   def prescription
     @patient = @current_doctor.patients.find params[:patient_id]
     @prescription = @patient.prescriptions.find params[:prescription_id]
-    @doses = @prescription.doses
+    @doses = @prescription.doses.order("created_at desc")
   end
 
   def new_prescription
